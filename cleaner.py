88a6bc744554cad1d24e37cb62f2b9fe70a2dd63
codefolder <codefolder@users.noreply.github.com>
2015-07-25 15:57:37 +0200

    Ported GUI to Gtk 3

--- cleaner.py
+++ cleaner.py
@@ -1,60 +1,55 @@
+#/usr/bin/python3
+
 import os.path
 import pickle
 import random
-
+'''
 from tkinter import Button
 from tkinter import END
 from tkinter import Entry
 from tkinter import Frame
 from tkinter import Label
 from tkinter import Tk
+'''
+# Replaced by Gtk
+
+from gi.repository import Gtk
+'''
 from tkinter import filedialog
 from tkinter import messagebox
+'''
 from psutil._compat import xrange
 
 __author__="codefolder@users.noreply.github.com"
 
-class Cleaner(Tk):
-    def __init__(self, parent):
-        Tk.__init__(self,parent)
+class Cleaner(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
         ################# class scope variable initialization ##############
-        self.cleanAllRequested = False # flag which is set when only a batch dialog is needed, supresses the dir confirm dialogs
+        self.cleanAllRequested = False # flag which is set when only a batch dialog is needed, supresses the dir confirm dialog
         self.settingsFileName = "cleaner.pickle"
-        self.__pathpanels__ = {} # __pathpanels__ = {intPanelNumber:[instanceOfPanelakaTkFrame, entryWidget]}
-        self.__persistentStorage__ = [] # [[panelNumber, entry.get()],[...]]        
-        ####################################################################
-        self.bind("<Escape>", lambda event: self.destroy())
-        self.bind("<Key-Escape>", lambda event: self.destroy())
-
-        self.mainFrm = Frame(self,name="mainFrm",relief="raised",border=1)
-        self.mainFrm.pack(fill="x",padx=3,pady=3)
+        self.__pathpanels__ = {} # __pathpanels__ = {intPanelNumber:[instanceOfPanel aka Gtk.Box, Gtk.Entry]}
+        self.__persistentStorage__ = [] # [[panelNumber, entry.get_text()],[...]]
         ################# class scope variable initialization ##############
+        self.box = Gtk.Box(spacing=10, orientation=Gtk.Orientation.VERTICAL)
         self.restoreState() # __pathpanels__
-        ####################################################################        
-        buttonAddDir = Button(self,text="Add new", command=lambda: self.createNewPathPanel(addNewPanel=True))
-        buttonCleanAll = Button(self,text="Clean all", command=lambda: self.purgeAllDirectories())
-        buttonCleanAll.pack(side="right",anchor="n",padx=20,pady=8)
-        buttonAddDir.pack(side="right",anchor="n",padx=0,pady=8)
-
-        self.center()      
+        ####################################################################
+        self.buttonAddDir = Gtk.Button(label="Add new")
+        self.buttonCleanAll = Gtk.Button(label="Clean all")
+        self.buttonAddDir.connect("clicked", lambda wid: self.createNewPathPanel(addNewPanel=True))
+        self.buttonCleanAll.connect("clicked", lambda wid: self.purgeAllDirectories())
+        panel = Gtk.Box(spacing=10)
+        panel.pack_start(Gtk.Box(), True, True, 0)
+        panel.pack_start(self.buttonCleanAll, False, True, 0)
+        panel.pack_start(self.buttonAddDir, False, True, 0)
+        self.box.add(panel)
+        self.add(self.box)
         try:
             settingsFile = open(self.settingsFileName,"rb")
             self.__persistentStorage__ = pickle.load(settingsFile)
             settingsFile.close()
         except IOError:
             print("Could not load settings file.")
- 
-
-    def center(self):
-        ############## center on screen ################        
-        self.update()        
-        ww, wh, w, h = self.winfo_screenwidth(), self.winfo_screenheight(), self.winfo_width(), self.winfo_height()
-        self.geometry("%dx%d+%d+%d" % (684,h,(ww-w)/2,(wh-h)/2))
-        ################################################
-
-    def destroy(self):
-        self.saveState()
-        self.quit()
 
     def restoreState(self):
         try:
@@ -73,25 +68,25 @@ class Cleaner(Tk):
             settingsFile.close()
         except IOError:
             print("Could not save settings file.")
-        
+
     def createNewPanelNumber(self):
         panelNumber = random.randint(0, 999999)
         while panelNumber in self.__pathpanels__:
             panelNumber = random.randint(0, 999999)
         return panelNumber
 
-    def processEvent(self, panelNumber, cmd):
+    def processEvent(self, wid, panelNumber, cmd):
         if panelNumber and cmd is not None:
-            entryWidget = self.__pathpanels__[panelNumber][1]
+            entry = self.__pathpanels__[panelNumber][1]
+            print ("--- Entry text reads ["+entry.get_text()+"] ---")
             ########################### debug ################################
-            print ("--- The order ["+cmd+"] is dispatched from panel number ["+str(panelNumber)+"] ---")
-            print ("--- Entry text reads ["+entryWidget.get()+"] ---")
+            print ("--- Command ["+cmd+"] is dispatched from panel number ["+str(panelNumber)+"] ---")
             ##################################################################
             if cmd == "Relinquish":
                 #################### remove panel from gui ###################
-                self.__pathpanels__[panelNumber][0].grid_forget()
+                self.box.remove(self.__pathpanels__[panelNumber][0])
                 self.__pathpanels__.pop(panelNumber)
-                self.geometry("") # re-pack toplevel
+                self.box.show_all() # re-pack toplevel
                 #################### remove panel from storage ###############
                 for i in xrange(0, len(self.__persistentStorage__)):
                     if self.__persistentStorage__[i][0]==panelNumber:
@@ -99,16 +94,26 @@ class Cleaner(Tk):
                         break
                 ##############################################################
                 return
-            
+
             if cmd == "Change":
                 #################### change path in gui ########################
-                currDir = entryWidget.get()
-                pathToDir = filedialog.askdirectory(initialdir=currDir,parent=self,title="Choose a directory to clean",mustexist=True)
-                if not pathToDir:
+                pathToDir = None
+                currDir = entry.get_text()
+                dialog = Gtk.FileChooserDialog("Please choose a folder", self,
+                                                Gtk.FileChooserAction.SELECT_FOLDER,
+                                                 (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                                               "Select", Gtk.ResponseType.OK))
+                dialog.set_current_folder(currDir)
+                response =  dialog.run()
+                if response == Gtk.ResponseType.OK:
+                    pathToDir = dialog.get_filename()
+                    dialog.destroy()
+                elif response == Gtk.ResponseType.CANCEL:
+                    dialog.destroy()
+                if not pathToDir: # file dialog cancelled or closed by user
                     return
                 else:
-                    entryWidget.delete(0, END)
-                    entryWidget.insert(0, pathToDir)
+                    entry.set_text(pathToDir)
                 #################### change path in storage ####################
                     for i in xrange(0, len(self.__persistentStorage__)):
                         if self.__persistentStorage__[i][0]==panelNumber:
@@ -116,56 +121,80 @@ class Cleaner(Tk):
                             break
             if cmd == "Clean":
                 ##### purge root directory of all files and subdirectories #####
-                rootDir = entryWidget.get()
+                rootDir = entry.get_text()
                 self.purgeDirectory(rootDir)
                 ################################################################
 
     def createNewPathPanel(self, panelNumber=None, pathToDir=None, addNewPanel=False):
-        usrhome = os.path.expanduser('~')
         if not pathToDir: # create panel from scratch
-            pathToDir = filedialog.askdirectory(initialdir=usrhome,parent=self,title="Choose a directory to clean",mustexist=True)
+            dialog = Gtk.FileChooserDialog("Please choose a folder", self,
+                                            Gtk.FileChooserAction.SELECT_FOLDER,
+                                             (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                                           "Select", Gtk.ResponseType.OK))
+            response =  dialog.run()
+            if response == Gtk.ResponseType.OK:
+                pathToDir = dialog.get_filename()
+                dialog.destroy()
+            elif response == Gtk.ResponseType.CANCEL:
+                dialog.destroy()
             if not pathToDir: # file dialog cancelled or closed by user
                 return
         if not panelNumber: # continue creating panel from scratch
             panelNumber = self.createNewPanelNumber()
         # in this section it does not matter if the panel is created from scratch or not - the same execution code
-        frm = Frame(self.mainFrm,name=str(panelNumber),relief="raised",border=1)
-        frm.grid()
-        label = Label(frm, text="Path to directory: ")
-        entry = Entry(frm, width=40)
-        entry.insert(0, pathToDir)
-        buttonRemove = Button(frm,text="Relinquish",command=lambda pn=panelNumber,cmd="Relinquish": self.processEvent(pn,cmd))
-        buttonChange = Button(frm,text="Change",command=lambda pn=panelNumber,cmd="Change": self.processEvent(pn,cmd))
-        buttonClean = Button(frm,text="Clean",command=lambda pn=panelNumber,cmd="Clean": self.processEvent(pn,cmd))
-        # The following lines recently started to cause a GUI geometry conflict
-        #label.pack(side="left",anchor="w",padx=1,pady=1)
-        #entry.pack(side="left",fill="x",expand="True",padx=1,pady=1)
-        label.grid(row=0,padx=1,pady=1)
-        entry.grid(row=0,column=1,padx=1,pady=1)
-        buttonRemove.grid(row=0,column=2,padx=1,pady=1)
-        buttonChange.grid(row=0,column=3,padx=1,pady=1)
-        buttonClean.grid(row=0,column=4,padx=1,pady=1)
-        self.__pathpanels__[panelNumber] = [frm, entry]
+        label = Gtk.Label("Path to directory: ")
+        entry = Gtk.Entry()
+        entry.set_hexpand(True)
+        entry.set_text(pathToDir)
+        buttonRemove = Gtk.Button(label="Relinquish")
+        buttonChange = Gtk.Button(label="Change")
+        buttonClean = Gtk.Button(label="Clean")
+        buttonRemove.connect("clicked", lambda wid, pn=panelNumber, cmd="Relinquish": self.processEvent(wid, pn, cmd))
+        buttonChange.connect("clicked", lambda wid, pn=panelNumber, cmd="Change": self.processEvent(wid, pn,cmd))
+        buttonClean.connect("clicked", lambda wid, pn=panelNumber, cmd="Clean": self.processEvent(wid, pn, cmd))
+        panel = Gtk.Box(spacing=4)
+        panel.set_name(str(panelNumber))
+        panel.add(label)
+        panel.add(entry)
+        panel.add(buttonRemove)
+        panel.add(buttonChange)
+        panel.add(buttonClean)
+        self.box.add(panel)
+        self.box.show_all()
+        self.__pathpanels__[panelNumber] = [panel, entry]
         if addNewPanel:
-            self.__persistentStorage__.append([panelNumber,entry.get()])
-            self.geometry("") # some trick! not to be found in any docs! needs a null parameter to re-pack.
+            self.__persistentStorage__.append([panelNumber,entry.get_text()])
 
     def purgeAllDirectories(self):
-        confirmed = messagebox.askyesno(
-            message="Clean all directories by deleting all files in them?",
-            icon="question", title="Final sanity check");
+        confirmed = False
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.QUESTION,
+                               Gtk.ButtonsType.YES_NO, "Final sanity check")
+        dialog.format_secondary_text("Clean all directories by deleting all files in them?")
+        response = dialog.run()
+        if response == Gtk.ResponseType.YES:
+            confirmed = True
+        elif response == Gtk.ResponseType.NO:
+            pass 
+        dialog.destroy()
         if not confirmed: return
         self.cleanAllRequested = True
         for i in self.__pathpanels__.keys():
-            self.purgeDirectory(self.__pathpanels__[i][1].get())
+            self.purgeDirectory(self.__pathpanels__[i][1].get_text())
         self.cleanAllRequested = False
 
-    def purgeDirectory(self,  rootDir=None):
+    def purgeDirectory(self, rootDir=None):
         if rootDir == None: return
+        confirmed = False
         if self.cleanAllRequested == False: # not to bring up dialog for every dir in a batch
-            confirmed = messagebox.askyesno(
-                message="Delete all files in this directory?",
-                icon="question", title="Final sanity check");
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.QUESTION,
+                                   Gtk.ButtonsType.YES_NO, "Final sanity check")
+            dialog.format_secondary_text("Delete all files in this directory?")
+            response = dialog.run()
+            if response == Gtk.ResponseType.YES:
+                confirmed = True
+            elif response == Gtk.ResponseType.NO:
+               pass 
+            dialog.destroy()
             if not confirmed: return
         if os.access(rootDir,  os.F_OK):
             print("Path ["+rootDir+"] exists, ok.")
@@ -185,10 +214,15 @@ class Cleaner(Tk):
                     os.rmdir(os.path.join(root, dirName)) # this is dangerous
         else:
             print("...oops, ran into a problem: no write access to directory! Sorry...")
-if __name__ == "__main__":
-    app = Cleaner(None)
-    app.resizable(False, False)
-    app.title("Cleaner - v1.1")
-    app.center()
-    app.mainloop()
     
+    def saveAndExit(self, wid, gdkevent):
+        self.saveState()
+        Gtk.main_quit()
+
+if __name__ == "__main__":
+    win = Cleaner()
+    win.set_default_size(600, 0)
+    win.set_position(Gtk.WindowPosition.CENTER)
+    win.connect("delete-event", win.saveAndExit)
+    win.show_all()
+    Gtk.main()
